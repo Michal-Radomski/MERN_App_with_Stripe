b0234c266e6e026380d94bf15a3cf108b4c4219d
Michal Radomski <michalra@op.pl>
2022-07-02 22:47:11 +0200

    client - initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+
+function App(): JSX.Element {
+  return (
+    <React.Fragment>
+      <div className="App">App</div>
+    </React.Fragment>
+  );
+}
+
+export default App;
