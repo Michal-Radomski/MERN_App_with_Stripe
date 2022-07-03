4f5e6910012721387cc826fcc6a799521daba680
Michal Radomski <michalra@op.pl>
2022-07-03 03:25:51 +0200

    Hero.tsx done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
 import "./App.scss";
 
 function App(): JSX.Element {
@@ -8,6 +9,7 @@ function App(): JSX.Element {
     <React.Fragment>
       <div className="App">
         <Header />
+        <Hero />
       </div>
     </React.Fragment>
   );
---
+++ client/src/components/hero/Hero.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./Hero.styles.scss";
+
+const Hero = () => {
+  return (
+    <React.Fragment>
+      <section className="hero is-large is-info hero-image">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">Bags re-imagined for life.</h1>
+          </div>
+          <div className="show-now-btn">
+            <button className="button is-black" id="shop-now">
+              Shop Now
+            </button>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default Hero;
