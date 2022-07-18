b7b9f9941f82f1375425751d2117021c22be271c
Michal Radomski <michalra@op.pl>
2022-07-19 00:07:08 +0200

    Frontend - Stripe Setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter as Router} from "react-router-dom";
+import {Elements} from "@stripe/react-stripe-js";
+import {loadStripe} from "@stripe/stripe-js";
 
 import ProductsContextProvider from "./context/ProductsContext";
 import CartContextProvider from "./context/CartContext";
-
 import App from "./App";
+
+const stripeKey = process.env.REACT_APP_PUBLISHABLE_STRIPE_KEY as string;
+// console.log({stripeKey});
+
+const stripePromise = loadStripe(stripeKey);
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
@@ -14,7 +20,9 @@ root.render(
     <Router>
       <ProductsContextProvider>
         <CartContextProvider>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </CartContextProvider>
       </ProductsContextProvider>
     </Router>
