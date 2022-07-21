cbc53ef6f7bc883f9879cd3a1ea08a605a4aa220
Michal Radomski <michalra@op.pl>
2022-07-21 23:59:42 +0200

    ShippingAddress component done

--- client/src/components/checkout/Checkout.tsx
+++ client/src/components/checkout/Checkout.tsx
@@ -2,11 +2,18 @@ import React from "react";
 
 import {CartContext} from "../../context/CartContext";
 import Layout from "../shared/Layout";
-import StripeCheckout from "./stripe-checkout/StripeCheckout";
+// import StripeCheckout from "./stripe-checkout/StripeCheckout";
+import ShippingAddress from "./custom-checkout/ShippingAddress";
 import "./Checkout.styles.scss";
 
 const Checkout = (): JSX.Element => {
   const {itemCount, total} = React.useContext(CartContext as any);
+
+  const [shipping, setShipping] = React.useState({});
+
+  const addressShown = {
+    display: shipping ? "none" : "block",
+  };
 
   return (
     <Layout>
@@ -14,7 +21,10 @@ const Checkout = (): JSX.Element => {
         <h2>Checkout Summary</h2>
         <h3>{`Total Items: ${itemCount}`}</h3>
         <h4>{`Amount to Pay: $ ${total}`}</h4>
-        <StripeCheckout />
+        {/* <StripeCheckout /> */}
+        <div style={addressShown}>
+          <ShippingAddress setShipping={setShipping} />
+        </div>
       </div>
     </Layout>
   );
---
+++ client/src/components/checkout/custom-checkout/ShippingAddress.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import {Formik} from "formik";
+
+const validate = (values: {name: string; email: string; address: string}) => {
+  const {name, email, address} = values;
+  const errors: {email: string; address: string; name: string} = {
+    email: "",
+    address: "",
+    name: "",
+  };
+  if (!email) {
+    errors.email = "Required";
+  }
+  if (!name) {
+    errors.name = "Required";
+  }
+  if (!address) {
+    errors.address = "Required";
+  }
+  return errors;
+};
+
+const ShippingAddress = ({
+  setShipping,
+}: {
+  setShipping: (values: {email: string; address: string; name: string}) => void;
+}): JSX.Element => {
+  const initialValues = {
+    email: "",
+    name: "",
+    address: "",
+  };
+
+  return (
+    <React.Fragment>
+      <div>
+        <h4>Shipping Address</h4>
+        <Formik
+          initialValues={initialValues}
+          validate={validate}
+          onSubmit={(values) => {
+            console.log("values", values);
+            setShipping(values);
+          }}
+        >
+          {({values, errors, handleChange, handleSubmit}) => {
+            const {name, email, address} = errors;
+            return (
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <input
+                    type="text"
+                    name="name"
+                    onChange={handleChange}
+                    value={values.name}
+                    placeholder="Name"
+                    className={"nomad-input " + (name ? "error" : "")}
+                  />
+                </div>
+                <div>
+                  <input
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    value={values.email}
+                    placeholder="Email"
+                    className={"nomad-input " + (email ? "error" : "")}
+                  />
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    name="address"
+                    onChange={handleChange}
+                    value={values.address}
+                    placeholder="Address"
+                    className={"nomad-input " + (address ? "error" : "")}
+                  />
+                </div>
+                <div className="submit-btn">
+                  <button type="submit" className="button is-black nomad-btn submit">
+                    Continue
+                  </button>
+                </div>
+              </form>
+            );
+          }}
+        </Formik>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default ShippingAddress;
