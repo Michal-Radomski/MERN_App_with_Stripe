154cfbe2295ea03c70a8148fadab1af0901220e2
Michal Radomski <michalra@op.pl>
2022-07-24 23:01:22 +0200

    Firebase - index.ts updated

--- client/src/firebase/index.ts
+++ client/src/firebase/index.ts
@@ -16,4 +16,84 @@ firebase.initializeApp(firebaseConfig);
 const firestore = firebase.firestore();
 const auth = firebase.auth();
 
-export {firestore, auth};
+interface CustomError {
+  message: string;
+}
+
+const createUserProfileDocument = async (
+  userAuth: {uid?: string; displayName?: string; email?: string},
+  additionalData: firebase.firestore.DocumentData
+) => {
+  console.log({userAuth, additionalData});
+
+  if (!userAuth) {
+    return null;
+  }
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`); // e.g. users/uniq12345
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName: displayName,
+        email: email,
+        createdAt: createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", (error as CustomError).message);
+    }
+  }
+  return userRef;
+};
+
+export {firestore, createUserProfileDocument, auth};
+
+//* For Version 9
+// import firebase from "firebase/compat/app";
+// import "firebase/compat/firestore";
+
+// const config = {
+//   apiKey: "",
+//   authDomain: "",
+//   projectId: "",
+//   storageBucket: "",
+//   messagingSenderId: "",
+//   appId: "",
+// };
+
+// firebase.initializeApp(config);
+
+// const firestore = firebase.firestore();
+
+// const auth = firebase.auth();
+
+// const createUserProfileDocument = async (userAuth, additionalData) => {
+//   if (!userAuth) {
+//     return;
+//   }
+
+//   const userRef = firestore.doc(`users/${userAuth.multiFactor.user.uid}`);
+
+//   const snapShot = await userRef.get();
+
+//   if (!snapShot.exists) {
+//     const {displayName, email} = userAuth;
+
+//     const createdAt = new Date();
+
+//     try {
+//       await userRef.set({displayName, email, createdAt, ...additionalData});
+//     } catch (error) {
+//       console.log("error creating user", error.message);
+//     }
+//   }
+
+//   return userRef;
+// };
+
+// export {firestore, createUserProfileDocument, auth};
