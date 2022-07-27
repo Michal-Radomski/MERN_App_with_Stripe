dd26762500ae7db45d7522b87738a0bcc66b221f
Michal Radomski <michalra@op.pl>
2022-07-28 00:02:46 +0200

    setupIntent done

---
+++ server/API/setupIntent.ts
@@ -0,0 +1,30 @@
+export {};
+import {Request, Response} from "express";
+
+const stripeAPI = require("../stripe");
+const getCustomer = require("../helpers/getCustomer");
+
+interface CustomRequest extends Request {
+  currentUser: any;
+}
+
+async function setupIntent(req: CustomRequest, res: Response) {
+  const {currentUser} = req;
+  // Get Stripe Customer
+  const customer = await getCustomer(currentUser.uid);
+  console.log({customer});
+  let setupIntent;
+
+  try {
+    setupIntent = await stripeAPI.setupIntents.create({
+      customer: customer.id,
+    });
+    console.log({setupIntent});
+    res.status(200).json(setupIntent);
+  } catch (error) {
+    console.log({error});
+    res.status(400).json({error: "an error occurred, unable to create setup intent"});
+  }
+}
+
+module.exports = setupIntent;
